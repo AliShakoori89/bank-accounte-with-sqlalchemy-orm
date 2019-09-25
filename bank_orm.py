098b90d6d348aed79e240f058ac36ad4c2e4b23e
unknown <alishakoori89@gmail.comZCLEAR close>
2019-09-25 21:08:50 +0330

    edit code

--- bank_orm.py
+++ bank_orm.py
@@ -1,57 +1,81 @@
 import sqlalchemy
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-global i
-i=0
+
 Base = declarative_base()
-engine = create_engine('sqlite:///bank.db', echo=True)
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-class Customer(Base):
+
+
+class Customer(Base):   
     __tablename__ ="customer"
     id = Column(Integer, primary_key=True)
     firstname = Column(String)
     lastname = Column(String)
     bankaccount = Column(Integer)
     supply = Column(Integer,nullable=False)
-    # def __init__(self,id,firstname,lastname,bankaccount,supply):
-    #     self.id=id
-    #     self.firstname=firstname
-    #     self.lastname=lastname
-    #     self.bankaccount=bankaccount
-    #     self.supply=supply
+    def __init__(self,id,firstname,lastname,bankaccount,supply):
 
+        self.engine = create_engine('sqlite:///bank.db', echo=True)
+        DBSession = sessionmaker(bind=self.engine)
+        self.session = DBSession()
 
     def insert_table(self,data_list,id_field):
-        global i
-        
-        customers = session.query(Customer).all()
+        i=0
+        Base.metadata.create_all(self.engine)
+        customers = self.session.query(Customer).all()
         for customer in customers:
-            # print(f'{customer.id} was released on {customer.firstname}')
             if id_field == customer.id:
                 return False
-            
-        Base.metadata.create_all(engine)
         customer1=Customer(id=data_list[i],firstname=data_list[i+1],lastname=data_list[i+2],bankaccount=data_list[i+3],supply=data_list[i+4])
-        session.add(customer1)
-        session.commit()
+        self.session.add(customer1)
+        self.session.commit()
         return True
         
-    
     def open_search(self,id_search):
-        customers = session.query(Customer).all()
-        for customer in customers:
-            if id_search == customer.id:
-                return customer.id
-    
-    def add_sheets(self,intended_data,id_search,sheet):
-        l=list(intended_data)
-        new_suply=l[0]+sheet
-        customer=session.query(Customer).filter(id==id_search).first()
-        print(customer.fetchall())
-
-
+        intended_supply = self.session.query(Customer.supply).filter(Customer.id == id_search)
+        for row in intended_supply: 
+            if row != 0:
+                return intended_supply
+            else:
+                return False
         
+    def add_sheets(self,intended_data,id_search,sheet):
+        for row in intended_data:
+            select_row=list(row)
+            sheet+=select_row[0]
+            self.session.query(Customer).filter(Customer.id == id_search).update({Customer.supply: sheet})
+            self.session.commit()
+
+    def sub_sheets(self,intended_data,id_search,sheet):
+        for row in intended_data:
+            select_row=list(row)
+            select_row[0]-=sheet
+            if select_row[0]>0 :
+                self.session.query(Customer).filter(Customer.id == id_search).update({Customer.supply: select_row[0]})
+                self.session.commit()
+                return True
+            else:
+                return False
+
+    def withdraw(self,id_search):
+        if self.session.query(Customer).filter(Customer.id == id_search).delete():
+            self.session.commit()
+            return True
+        else:
+            return False
+
+    def show(self):
+        list_user=[]
+        all_list_user=[]
+        data=self.session.query(Customer).all()
+
+        for row in data:
+            list_user.append(row.id)
+            list_user.append(row.firstname)
+            list_user.append(row.lastname)
+            list_user.append(row.bankaccount)
+            list_user.append(row.supply)
+        all_list_user.append(list_user)
+        return all_list_user
